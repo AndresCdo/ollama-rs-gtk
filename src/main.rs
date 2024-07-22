e807f2464907187ffbbc387fd66c642524daeaf5
Andres Caicedo <andrezz1997@gmail.com>
2024-07-22 15:16:25 +0200

    chore: Update main.rs to improve UI responsiveness

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use gtk::{Application, ApplicationWindow, Button, Entry, TextView, ScrolledWindow, Orientation};
+use gtk::{Application, ApplicationWindow, Button, Entry, Orientation, ScrolledWindow, TextView};
 use reqwest::Client;
 use serde_json::{json, Value};
 use std::rc::Rc;
@@ -6,7 +6,7 @@ use std::cell::RefCell;
 use std::time::Duration;
 use tokio::runtime::Runtime;
 use glib::MainContext;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use gtk::prelude::*;
 
@@ -38,15 +38,21 @@ fn main() {
         let window = ApplicationWindow::new(app);
         window.set_title("Ollama Llama 3 Assistant");
         window.set_default_size(400, 300);
+        
+        let hbox = gtk::Box::new(Orientation::Horizontal, 5);
+        window.add(&hbox);
 
         let vbox = gtk::Box::new(Orientation::Vertical, 5);
-        window.add(&vbox);
+        hbox.pack_start(&vbox, true, true, 5);
 
         let input_entry = Rc::new(RefCell::new(Entry::new()));
         vbox.pack_start(&*input_entry.borrow(), false, false, 0);
 
         let send_button = Button::with_label("Send");
         vbox.pack_start(&send_button, false, false, 0);
+
+        // Shared state to control button sensitivity across threads
+        let button_state = Arc::new(Mutex::new(send_button.clone()));
 
         let scrolled_window = ScrolledWindow::new(None::<&gtk::Adjustment>, None::<&gtk::Adjustment>);
         scrolled_window.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
@@ -69,27 +75,36 @@ fn main() {
         send_button.connect_clicked(move |_| {
             let prompt = input_entry_clone.borrow().text().to_string();
             let prompt_clone = prompt.clone();
+
+            let  button = button_state.lock().unwrap();
+            button.set_sensitive(false);
+
+            let button_state_clone = Arc::clone(&button_state);
+
             if !prompt.is_empty() {
                 let text_buffer_clone = Rc::clone(&text_buffer_clone);
                 let input_entry_clone = Rc::clone(&input_entry_clone);
                 let runtime = runtime.clone();
 
                 main_context.spawn_local(async move {
+                    let button = button_state_clone.lock().unwrap();
                     match runtime.spawn(async move {
                         send_prompt(&prompt).await.unwrap()
                     }).await {
                         Ok(response) => {
                             let text_buffer = text_buffer_clone.borrow_mut();
                             let mut end_iter = text_buffer.end_iter();
-                            text_buffer.insert_markup(&mut end_iter, &format!("You: {}\n", prompt_clone));
-                            text_buffer.insert_markup(&mut end_iter, &format!("Ollama: {}\n", response));
+                            text_buffer.insert_markup(&mut end_iter, &format!("You: {}\n\n", prompt_clone));
+                            text_buffer.insert_markup(&mut end_iter, &format!("Ollama: {}\n\n", response));
                         },
                         Err(e) => {
                             let text_buffer = text_buffer_clone.borrow_mut();
                             let mut end_iter = text_buffer.end_iter();
-                            text_buffer.insert_markup(&mut end_iter, &format!("Error: {}\n", e));
+                            text_buffer.insert_markup(&mut end_iter, &format!("Error: {}\n\n", e));
                         }
                     }
+
+                    button.set_sensitive(true);
                 });
                 input_entry_clone.borrow_mut().set_text("");
             }
